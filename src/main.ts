ddec12159c531e79793cdf462e0651a992834417
Vanessa Phipps <vanessa.phipps@cfa.harvard.edu>
2024-09-30 19:26:10 -0400

    Add component registering

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
 import { ViteSSG } from "vite-ssg";
 import App from "./App.vue";
 import PlaygroundView from "./pages/PlaygroundView.vue";
+import { createBootstrap } from "bootstrap-vue-next";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-next/dist/bootstrap-vue-next.css";
 
 const routes = [{ path: "/", component: PlaygroundView }];
 
-export const createApp = ViteSSG(App, { routes });
+export const createApp = ViteSSG(
+  App,
+  { routes },
+  ({ app, router, routes, isClient, initialState }) => {
+    app.use(createBootstrap());
+  },
+);
