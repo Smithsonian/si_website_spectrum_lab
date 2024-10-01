b991fff1ff73ad2b00a434dbe5ab0d3aa3ff4142
Vanessa Phipps <vanessa.phipps@cfa.harvard.edu>
2024-10-01 13:38:27 -0400

    Nav styling

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <div class="text-center top-banner">
+  <div class="top-banner">
     <h1>Spectrum Lab</h1>
-    <p>Exploring the world of color</p>
+    <p class="m-0">Exploring the world of color</p>
   </div>
-  <BNav>
-    <BNavItem href="session2.html">Session 2</BNavItem>
-    <BNavItem href="session3.html">Session 3</BNavItem>
-    <BNavItem href="session4.html">Session 4</BNavItem>
-    <BNavItem href="fishtank.html">Fish Tank</BNavItem>
-    <BNavItem href="exoplanets.html">Exoplanets</BNavItem>
-    <BNavItem href="playground.html">Playground</BNavItem>
-    <BNavItem to="/">Vue Playground</BNavItem>
-    <BNavItem to="/session2vue">Vue Session 2</BNavItem>
+  <BNav class="bg-dark justify-content-center">
+    <BNavItem link-class="app-nav-link" href="session2.html"
+      >Session 2</BNavItem
+    >
+    <BNavItem link-class="app-nav-link" href="session3.html"
+      >Session 3</BNavItem
+    >
+    <BNavItem link-class="app-nav-link" href="session4.html"
+      >Session 4</BNavItem
+    >
+    <BNavItem link-class="app-nav-link" href="fishtank.html"
+      >Fish Tank</BNavItem
+    >
+    <BNavItem link-class="app-nav-link" href="exoplanets.html"
+      >Exoplanets</BNavItem
+    >
+    <BNavItem link-class="app-nav-link" href="playground.html"
+      >Playground</BNavItem
+    >
+    <BNavItem link-class="app-nav-link" to="/">Vue Playground</BNavItem>
+    <BNavItem link-class="app-nav-link" to="/session2vue"
+      >Vue Session 2</BNavItem
+    >
   </BNav>
   <main>
     <RouterView />
   </main>
 </template>
 
-<style scoped>
+<style>
 .top-banner {
+  text-align: center;
   background: url(/includes/AI_common/images/rainbow-line-art_H210.png) center
     left no-repeat;
+}
+.nav-link.app-nav-link {
+  color: #999;
+  transition: ease-in-out color 0.15s;
+}
+.nav-link.app-nav-link:hover {
+  color: #fff;
+}
+.nav-link.router-link-active,
+.nav-link.router-link-active:hover,
+.nav-link.router-link-active:focus {
+  color: #479ff8;
 }
 </style>
