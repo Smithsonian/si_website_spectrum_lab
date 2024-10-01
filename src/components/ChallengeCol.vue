923ead025e41be91a4d54621a55ac76cabb02692
Vanessa Phipps <vanessa.phipps@cfa.harvard.edu>
2024-10-01 15:47:50 -0400

    Challenge copy

--- src/components/ChallengeCol.vue
+++ src/components/ChallengeCol.vue
@@ -2,6 +2,11 @@
   <BCol lg="4" class="mb-3">
     <div class="p-2 rounded-4 border border-dark">
       <h2>Welcome to the Playground</h2>
+      <p>
+        This Spectrum Tool will help you connect the visual color display of a
+        light source's spectrum&mdash;like the color image you see in a
+        spectroscope&mdash;with a graph of the light's spectrum.
+      </p>
     </div>
   </BCol>
 </template>
