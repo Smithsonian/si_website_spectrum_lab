282ed6e076fc0e81f92707eeb5a3d99eb874efc4
Vanessa Phipps <vanessa.phipps@cfa.harvard.edu>
2024-10-02 11:28:06 -0400

    Filepicker

--- src/components/ChallengeCol.vue
+++ src/components/ChallengeCol.vue
@@ -1,6 +1,6 @@
 <template>
   <BCol lg="4" class="mb-3">
-    <div class="p-2 rounded-4 border border-dark">
+    <div class="p-3 rounded-4 border border-dark">
       <h2>Welcome to the Playground</h2>
       <p>
         This Spectrum Tool will help you connect the visual color display of a
--- src/components/ToolCol.vue
+++ src/components/ToolCol.vue
@@ -1,16 +1,53 @@
 <template>
   <BCol>
-    <div class="tool-col text-white p-2 rounded-4">
-      <div class="d-flex align-items-center">
-        <BButton>Upload your own spectrum data (csv)</BButton>
-        <div class="ms-2">No file selected for upload</div>
-      </div>
+    <div class="tool-col text-white p-3 rounded-4">
+      <BRow>
+        <BCol cols="8" md="6" lg="8" xl="6">
+          <BFormGroup label="Use your own spectrum data (CSV)">
+            <BFormFile
+              v-model="file"
+              accept="text/csv"
+              class="spectrum-filepicker"
+            />
+          </BFormGroup>
+        </BCol>
+        <BCol />
+      </BRow>
     </div>
   </BCol>
 </template>
 
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const file = ref<null | File>(null);
+</script>
+
 <style>
 .tool-col {
   background-color: #1e4c7d;
+}
+input.spectrum-filepicker {
+  background-color: rgb(255 255 255 / 10%);
+  color: var(--bs-light);
+  border-color: var(--bs-gray);
+}
+input.spectrum-filepicker::file-selector-button {
+  color: var(--bs-light);
+  background-color: #7f9ccb;
+}
+input.spectrum-filepicker:focus:not(:disabled):not(
+    [readonly]
+  )::file-selector-button,
+input.spectrum-filepicker:hover:not(:disabled):not(
+    [readonly]
+  )::file-selector-button {
+  color: var(--bs-light);
+  background-color: #9bbff8;
+}
+input.spectrum-filepicker:focus,
+input.spectrum-filepicker:hover {
+  background-color: rgb(255 255 255 / 30%);
+  color: var(--bs-light);
 }
 </style>
