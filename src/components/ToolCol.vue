26b80b4829dba1c68928689e2d0730bf44327e1a
Vanessa Phipps <vanessa.phipps@cfa.harvard.edu>
2024-10-02 12:19:40 -0400

    Spectrum icon

--- src/components/ToolCol.vue
+++ src/components/ToolCol.vue
@@ -1,7 +1,7 @@
 <template>
   <BCol>
     <div class="tool-col text-white p-3 rounded-4">
-      <BRow>
+      <BRow class="mb-3">
         <BCol cols="8" md="6" lg="8" xl="6">
           <BFormGroup label="Use your own spectrum data (CSV)">
             <BFormFile
@@ -13,6 +13,18 @@
         </BCol>
         <BCol />
       </BRow>
+      <BRow>
+        <BCol>
+          <div class="h3">Spectrum 1</div>
+          <div class="spectrum-icon-holder rounded-4">
+            <img
+              class="spectrum-icon"
+              :src="`/includes/AI_common/images/${iconFilename}`"
+            />
+          </div>
+        </BCol>
+        <BCol></BCol>
+      </BRow>
     </div>
   </BCol>
 </template>
@@ -21,6 +33,7 @@
 import { ref } from 'vue';
 
 const file = ref<null | File>(null);
+const iconFilename = ref('Harry_sun_spectrum.jpg');
 </script>
 
 <style>
@@ -49,5 +62,13 @@ input.spectrum-filepicker:focus,
 input.spectrum-filepicker:hover {
   background-color: rgb(255 255 255 / 30%);
   color: var(--bs-light);
+}
+.spectrum-icon-holder {
+  height: 160px;
+  width: 180px;
+  overflow: hidden;
+}
+.spectrum-icon {
+  margin-left: -30px;
 }
 </style>
