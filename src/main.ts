b3f16f205382a8bb51327a031320c7aced95e5e1
Vanessa Phipps <vanessa.phipps@cfa.harvard.edu>
2024-10-21 17:43:29 -0400

    main.ts: rearrange imports

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { ViteSSG } from 'vite-ssg';
-import App from './App.vue';
-import PlaygroundView from './pages/PlaygroundView.vue';
 import { createBootstrap } from 'bootstrap-vue-next';
-
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
 import VueGtag from 'vue-gtag';
-import Session2View from './pages/Session2View.vue';
 import type { RouteRecordRaw } from 'vue-router';
 import { createPinia } from 'pinia';
+
+import App from './App.vue';
+import PlaygroundView from './pages/PlaygroundView.vue';
+import Session2View from './pages/Session2View.vue';
 import { useMetadataStore } from './metadataStore';
 
 const routes: RouteRecordRaw[] = [
