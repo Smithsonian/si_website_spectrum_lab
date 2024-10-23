006b38897df140da2e2b7a0dcdf29033c3d71e60
Vanessa Phipps <vanessa.phipps@cfa.harvard.edu>
2024-10-23 19:05:27 -0400

    Refactor labels inside ticks

--- src/components/BottomAxis.vue
+++ src/components/BottomAxis.vue
@@ -33,12 +33,18 @@ const drawAxis = (zoom: number) => {
   // Clear
   ctx.clearRect(0, 0, xCanvasWidth, 30);
 
-  // Small ticks
+  // Configure font
+  ctx.font = '11px Arial';
+  ctx.fillStyle = 'white';
+
+  // 1/10 micron ticks
   ctx.lineWidth = 1;
+  let smallStart = 2;
   if (zoom >= 0.08) {
-    for (let i = 0; i < 98; i++) {
+    for (let i = smallStart; i < 100; i++) {
       const xTickDistance = 100;
-      const xTickPosition = xLeftSideRoom + i * xTickDistance * pixelZoom;
+      const xTickPosition =
+        xLeftSideRoom + (i - smallStart) * xTickDistance * pixelZoom;
       // Canvas lines 1 pixel thick are only sharp when 0.5 offset from the grid
       const xTickPosSharp = Math.floor(xTickPosition) + 0.5;
       if (xTickPosSharp > xCanvasWidth) {
@@ -48,6 +54,13 @@ const drawAxis = (zoom: number) => {
       ctx.moveTo(xTickPosSharp, 0);
       ctx.lineTo(xTickPosSharp, 4);
       ctx.stroke();
+
+      // 1/10 micron labels. Skip whole numbers
+      const wavelength = i / 10;
+      if (zoom > 0.4 && wavelength !== Math.floor(wavelength)) {
+        const xTextOffset = 5;
+        ctx.fillText(`${wavelength}`, xTickPosition - xTextOffset, 18);
+      }
     }
   } else {
     // Draw zero-ish tick
@@ -78,33 +91,15 @@ const drawAxis = (zoom: number) => {
   }
 
   // Numbers
-  ctx.font = '11px Arial';
-  ctx.fillStyle = 'white';
-
-  // Numbers 0.2 to 0.9
-  let start = 2;
-  if (zoom > 0.5) {
-    for (let i = start; i <= 9; i++) {
-      const xSmallTickDistance = 100;
-      const xTickPosition =
-        xLeftSideRoom + (i - start) * xSmallTickDistance * pixelZoom;
-      if (xTickPosition > xCanvasWidth) {
-        break;
-      }
-      const xTextOffset = 5;
-      ctx.fillText(`${i / 10}`, xTickPosition - xTextOffset, 18);
-    }
-  }
-
   // Numbers 1 to 10
-  start = 1;
-  for (let i = start; i <= 10; i++) {
+  let mediumStart = 1;
+  for (let i = mediumStart; i <= 10; i++) {
     const xTickDistance = 1000;
     // Since the tenths start at 0.2, we need to start at 800 not 1000
     const xPreviousTicksOffset = 800;
     const xTickPosition =
       xLeftSideRoom +
-      (xPreviousTicksOffset + (i - start) * xTickDistance) * pixelZoom;
+      (xPreviousTicksOffset + (i - mediumStart) * xTickDistance) * pixelZoom;
     if (xTickPosition > xCanvasWidth) {
       break;
     }
