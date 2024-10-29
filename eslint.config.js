d09e70637795f30816f8eac0e8e33a799a317ce5
Vanessa Phipps <vanessa.phipps@cfa.harvard.edu>
2024-10-29 18:32:15 -0400

    ESLint: ignore underscore

--- eslint.config.js
+++ eslint.config.js
@@ -17,6 +17,20 @@ export default [
         parser: '@typescript-eslint/parser',
       },
     },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
   },
   pluginJs.configs.recommended,
   ...pluginTs.configs.recommended,
