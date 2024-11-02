97c36bdddde666750b55fc7225358265c90a3afd
Vanessa Phipps <vanessa.phipps@cfa.harvard.edu>
2024-11-01 21:17:41 -0400

    Drawing fast!!

--- src/components/SpectrumDrawing.vue
+++ src/components/SpectrumDrawing.vue
@@ -16,7 +16,11 @@
 
 <script setup lang="ts">
 import { CHART_HEIGHT, CHART_WIDTH, X_BUCKET_WIDTH } from '@/constants';
-import { createRefWithUpdater, drawnSpectrumDataKey } from '@/injectionKeys';
+import {
+  createRefWithUpdater,
+  drawnSpectrumDataKey,
+  zoomKey,
+} from '@/injectionKeys';
 import { bucketFromXLoc, yInRangeFromY } from '@/utils/drawingUtils';
 import { computed, inject, onMounted, ref, useTemplateRef, watch } from 'vue';
 const canvas = useTemplateRef('canvas');
@@ -45,34 +49,84 @@ onMounted(() => {
 });
 
 const currentlyDrawing = ref(false);
+const xMostRecent = ref<number | null>(null);
+const yMostRecent = ref<number | null>(null);
+watch(currentlyDrawing, (newCurrentlyDrawing) => {
+  if (!newCurrentlyDrawing) {
+    xMostRecent.value = null;
+    yMostRecent.value = null;
+  }
+});
 
-const drawBucket = (e: PointerEvent): void => {
+const drawPoint = (xLoc: number, yLoc: number): void => {
   const ctx = context.value;
   if (!ctx) {
     return;
   }
-  const bucket = bucketFromXLoc(e.offsetX);
+  const bucket = bucketFromXLoc(xLoc);
   // Clear this bucket's rectangle
   const xBucketStart = bucket * X_BUCKET_WIDTH;
   ctx.clearRect(xBucketStart, 0, X_BUCKET_WIDTH, CHART_HEIGHT);
 
   // Draw point at the center
   const xCenter = (bucket + 0.5) * X_BUCKET_WIDTH;
-  const yInRange = yInRangeFromY(e.offsetY);
+  const yInRange = yInRangeFromY(yLoc);
   ctx.beginPath();
   ctx.arc(xCenter, yInRange, 1.5, 0, 2 * Math.PI);
   ctx.fill();
+  xMostRecent.value = xCenter;
+  yMostRecent.value = yInRange;
+};
+
+const drawDrag = (xLoc: number, yLoc: number): void => {
+  const xRecent = xMostRecent.value;
+  const yRecent = yMostRecent.value;
+  if (xRecent === null || yRecent === null) {
+    drawPoint(xLoc, yLoc);
+    return;
+  }
+
+  const mostRecentBucket = bucketFromXLoc(xRecent);
+  const currentBucket = bucketFromXLoc(xLoc);
+  const bucketDifference = currentBucket - mostRecentBucket;
+  if (Math.abs(bucketDifference) <= 1) {
+    // No intermediate buckets to draw
+    drawPoint(xLoc, yLoc);
+    return;
+  }
+  // We do have intermediate buckets to draw
+  const xDiff = xLoc - xRecent;
+  const yDiff = yLoc - yRecent;
+  const slope = yDiff / xDiff;
+  const intercept = yLoc - slope * xLoc;
+  const drawBucket = (b: number): void => {
+    const xCenter = (b + 0.5) * X_BUCKET_WIDTH;
+    const yLine = xCenter * slope + intercept;
+    drawPoint(xCenter, yLine);
+  };
+  // Handle both left and right drags
+  if (mostRecentBucket < currentBucket) {
+    // right drag
+    for (let b = mostRecentBucket + 1; b <= currentBucket; b++) {
+      drawBucket(b);
+    }
+  } else {
+    // left drag
+    for (let b = mostRecentBucket - 1; b >= currentBucket; b--) {
+      drawBucket(b);
+    }
+  }
 };
 
 const handlePointerDown = (e: PointerEvent) => {
   e.preventDefault();
-  drawBucket(e);
+  drawPoint(e.offsetX, e.offsetY);
   currentlyDrawing.value = true;
 };
 const handlePointerMove = (e: PointerEvent) => {
   e.preventDefault();
   if (currentlyDrawing.value) {
-    drawBucket(e);
+    drawDrag(e.offsetX, e.offsetY);
   }
 };
 const handlePointerUp = (e: PointerEvent) => {
@@ -81,8 +135,10 @@ const handlePointerUp = (e: PointerEvent) => {
 };
 const handlePointerLeave = (e: PointerEvent) => {
   e.preventDefault();
-  drawBucket(e);
-  currentlyDrawing.value = false;
+  if (currentlyDrawing.value) {
+    drawDrag(e.offsetX, e.offsetY);
+    currentlyDrawing.value = false;
+  }
 };
 
 const { ref: drawnSpectrumData } = inject(
@@ -94,6 +150,10 @@ watch(drawnSpectrumData, (newDrawnSpectrumData) => {
   if (newDrawnSpectrumData.length === 0) {
     clearChart();
   }
+});
+const zoom = inject(zoomKey, ref(1));
+watch(zoom, () => {
+  clearChart();
 });
 </script>
 
