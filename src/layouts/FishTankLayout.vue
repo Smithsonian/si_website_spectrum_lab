6eec6a7842a95508aad9d674be20f344b1a83c43
Vanessa Phipps <vanessa.phipps@cfa.harvard.edu>
2025-02-27 17:48:39 -0500

    Fishtank: rearrange section nav

--- src/layouts/FishTankLayout.vue
+++ src/layouts/FishTankLayout.vue
@@ -1,56 +1,59 @@
 <template>
-  <nav>
-    <ul class="nav nav-pills mt-3">
-      <li class="nav-item">
-        <RouterLink class="nav-link" active-class="active" to="/fishtank/intro"
-          >1. Fishtank Intro</RouterLink
-        >
-      </li>
-      <li class="nav-item">
-        <RouterLink
-          class="nav-link"
-          active-class="active"
-          to="/fishtank/background-trees"
-          >2. Background Part I</RouterLink
-        >
-      </li>
-      <li class="nav-item">
-        <RouterLink
-          class="nav-link"
-          active-class="active"
-          to="/fishtank/background-comparison"
-          >3. Background Part IIa</RouterLink
-        >
-      </li>
-      <li class="nav-item">
-        <RouterLink
-          class="nav-link"
-          active-class="active"
-          to="/fishtank/background-reveal"
-          >4. Background Part IIb</RouterLink
-        >
-      </li>
-      <li class="nav-item">
-        <RouterLink
-          class="nav-link"
-          active-class="active"
-          to="/fishtank/light-your-tank"
-          >5. Light Your Tank</RouterLink
-        >
-      </li>
-      <li class="nav-item">
-        <RouterLink
-          class="nav-link"
-          active-class="active"
-          to="/fishtank/mood-lighting"
-          >6. Bonus: Mood Lighting</RouterLink
-        >
-      </li>
-    </ul>
-  </nav>
   <ChallengeToolLayout>
     <template #challenge-col>
-      <slot name="challenge-tab"></slot>
+      <BRow>
+        <BCol cols="3">
+          <nav>
+            <ul class="nav nav-pills flex-column mt-3">
+              <li class="nav-item">
+                <RouterLink
+                  class="section-nav-link nav-link"
+                  to="/fishtank/intro"
+                  >1. Fishtank Intro</RouterLink
+                >
+              </li>
+              <li class="nav-item">
+                <RouterLink
+                  class="section-nav-link nav-link"
+                  to="/fishtank/background-trees"
+                  >2. Background Part I</RouterLink
+                >
+              </li>
+              <li class="nav-item">
+                <RouterLink
+                  class="section-nav-link nav-link"
+                  to="/fishtank/background-comparison"
+                  >3. Background Part IIa</RouterLink
+                >
+              </li>
+              <li class="nav-item">
+                <RouterLink
+                  class="section-nav-link nav-link"
+                  to="/fishtank/background-reveal"
+                  >4. Background Part IIb</RouterLink
+                >
+              </li>
+              <li class="nav-item">
+                <RouterLink
+                  class="section-nav-link nav-link"
+                  to="/fishtank/light-your-tank"
+                  >5. Light Your Tank</RouterLink
+                >
+              </li>
+              <li class="nav-item">
+                <RouterLink
+                  class="section-nav-link nav-link"
+                  to="/fishtank/mood-lighting"
+                  >6. Bonus: Mood Lighting</RouterLink
+                >
+              </li>
+            </ul>
+          </nav>
+        </BCol>
+        <BCol>
+          <slot name="challenge-tab"></slot>
+        </BCol>
+      </BRow>
     </template>
     <template #tool-col>
       <slot name="tool-col"></slot>
