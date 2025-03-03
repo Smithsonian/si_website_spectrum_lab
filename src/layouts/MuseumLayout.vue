d7a455f3f6afe35971f827c20a35ea90a15da73a
Pat Udomprasert <pudompra@cfa.harvard.edu>
2025-03-03 16:12:52 -0500

    Change to left menu navigation for module

--- src/layouts/MuseumLayout.vue
+++ src/layouts/MuseumLayout.vue
@@ -1,19 +1,45 @@
 <template>
-  <nav>
-    <ul class="nav nav-pills mt-3">
-      <li class="nav-item">
-        <RouterLink class="nav-link" active-class="active" to="/museum/intro"
-          >1. Museum Intro</RouterLink
-        >
-      </li>
-    </ul>
-  </nav>
   <ChallengeToolLayout>
     <template #challenge-col>
-      <slot name="challenge-tab"></slot>
+      <BRow>
+        <BCol cols="3">
+          <nav>
+            <ul class="nav nav-pills flex-column">
+              <li class="nav-item">
+                <RouterLink
+                  class="section-nav-link nav-link"
+                  to="/museum/intro"
+                  >Science of Art Conservation</RouterLink
+                >
+              </li>
+              <li class="nav-item">
+                <RouterLink
+                  class="section-nav-link nav-link"
+                  to="/museum/paint-colors"
+                  >Paint Colors & Spectra</RouterLink
+                >
+              </li>
+            </ul>
+          </nav>
+        </BCol>
+        <BCol>
+          <ChallengeIntro
+            heading="Design a Museum Display"
+            :icon-path="museumIconUrl"
+            icon-alt="Museum module icon"
+          >
+            For this project, you'll set up a museum display that conveys how spectra are used in art conservation.
+          </ChallengeIntro>
+          <slot name="challenge-tab"></slot>
+        </BCol>
+      </BRow>
     </template>
     <template #tool-col>
       <slot name="tool-col"></slot>
     </template>
   </ChallengeToolLayout>
 </template>
+
+<script setup lang="ts">
+import museumIconUrl from '@/assets/SVG/museum_conservation_icon.svg';
+</script>
