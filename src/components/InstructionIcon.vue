69fcd963467b2ab72d692d784435aa0029157b35
Vanessa Phipps <vanessa.phipps@cfa.harvard.edu>
2025-03-07 16:38:29 -0500

    Temperature: tutorial instructions

---
+++ src/components/InstructionIcon.vue
@@ -0,0 +1,37 @@
+<template>
+  <img
+    :src="iconPath"
+    :style="{ height: '30px', 'padding-left': extraPadding }"
+    class="ms-lg-2 ms-xl-3 ms-xxl-4"
+    alt="{{ name }} icon"
+  />
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import tutorialIcon from '@/assets/SVG/tutorial_instruction.svg';
+import notebookIcon from '@/assets/SVG/notebook_instruction.svg';
+import toolIcon from '@/assets/SVG/tool_instruction.svg';
+
+type InstructionIconName = 'Tutorial' | 'Notebook' | 'Tool';
+const props = defineProps<{ name: InstructionIconName }>();
+const iconPath = computed(() => {
+  switch (props.name) {
+    case 'Tutorial':
+      return tutorialIcon;
+    case 'Notebook':
+      return notebookIcon;
+    case 'Tool':
+      return toolIcon;
+    default:
+      return '';
+  }
+});
+// Help line up the different icons
+const extraPadding = computed(() => {
+  if (props.name === 'Notebook') {
+    return '2px';
+  }
+  return '0';
+});
+</script>
--- src/pages/Temperature/TemperatureTutorial.vue
+++ src/pages/Temperature/TemperatureTutorial.vue
@@ -2,7 +2,48 @@
   <TemperatureLayout>
     <template #challenge-tab>
       <ChallengeCard>
-        <h2>Tutorial here</h2>
+        <BRow>
+          <BCol cols="1"></BCol>
+          <BCol><h2 class="h4">Tutorial</h2></BCol>
+        </BRow>
+        <BRow>
+          <BCol cols="1">
+            <InstructionIcon name="Tutorial" />
+          </BCol>
+          <BCol>
+            <p>
+              <strong>Spectrum Tool Introduction.</strong> Follow the tutorial
+              (below) to learn how to use the spectrum tool and review key
+              concepts.
+            </p>
+          </BCol>
+        </BRow>
+        <BRow>
+          <BCol cols="1">
+            <InstructionIcon name="Notebook" />
+          </BCol>
+          <BCol>
+            <p class="mb-2">
+              <strong>Spectrum Notebook.</strong> In the
+              <em>Temperature, Tutorial</em>
+              section, answer these questions:
+            </p>
+            <ol class="fw-light">
+              <li class="mb-2">
+                What is the relationship between the wavelength and energy of
+                light?
+              </li>
+              <li class="mb-2">
+                Why might it be useful to display a spectrum graph as a scatter
+                plot instead of a line chart?
+              </li>
+              <li>
+                What parts of the electromagnetic spectrum are not displayed on
+                the Spectrum Tool?
+              </li>
+            </ol>
+          </BCol>
+        </BRow>
       </ChallengeCard>
     </template>
     <template #tool-col>
