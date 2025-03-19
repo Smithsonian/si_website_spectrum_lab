5b7bd043f4c0f5fc0c7c857ac2dea096387b10eb
Vanessa Phipps <vanessa.phipps@cfa.harvard.edu>
2025-03-19 18:09:00 -0400

    Museum: section nav

--- src/layouts/MuseumLayout.vue
+++ src/layouts/MuseumLayout.vue
@@ -3,57 +3,29 @@
     <template #challenge-col>
       <BRow>
         <BCol cols="3">
-          <nav>
-            <ul class="nav nav-pills flex-column">
-              <li class="nav-item">
-                <RouterLink class="section-nav-link nav-link" to="/museum/intro"
-                  >Science of Art Conservation</RouterLink
-                >
-              </li>
-              <li class="nav-item">
-                <RouterLink
-                  class="section-nav-link nav-link"
-                  to="/museum/paint-colors"
-                  >Paint Colors & Spectra</RouterLink
-                >
-              </li>
-              <li class="nav-item">
-                <RouterLink
-                  class="section-nav-link nav-link"
-                  to="/museum/two-blues"
-                  >Two Blue Spectra</RouterLink
-                >
-              </li>
-              <li class="nav-item">
-                <RouterLink
-                  class="section-nav-link nav-link"
-                  to="/museum/identify-blues"
-                  >Identify Pigments</RouterLink
-                >
-              </li>
-              <li class="nav-item">
-                <RouterLink
-                  class="section-nav-link nav-link"
-                  to="/museum/other-blues"
-                  >Other Blues</RouterLink
-                >
-              </li>
-              <li class="nav-item">
-                <RouterLink
-                  class="section-nav-link nav-link"
-                  to="/museum/blue-filter"
-                  >Blue Filter</RouterLink
-                >
-              </li>
-              <li class="nav-item">
-                <RouterLink
-                  class="section-nav-link nav-link"
-                  to="/museum/infrared-filter"
-                  >Infrared Filter</RouterLink
-                >
-              </li>
-            </ul>
-          </nav>
+          <SectionNav>
+            <SectionNavItem to="/museum/intro">
+              Science of Art Conservation
+            </SectionNavItem>
+            <SectionNavItem to="/museum/paint-colors">
+              Paint Colors &amp; Spectra
+            </SectionNavItem>
+            <SectionNavItem to="/museum/two-blues">
+              Two Blue Spectra
+            </SectionNavItem>
+            <SectionNavItem to="/museum/identify-blues">
+              Identify Pigments
+            </SectionNavItem>
+            <SectionNavItem to="/museum/other-blues">
+              Other Blues
+            </SectionNavItem>
+            <SectionNavItem to="/museum/blue-filter">
+              Blue Filter
+            </SectionNavItem>
+            <SectionNavItem to="/museum/infrared-filter">
+              Infrared Filter
+            </SectionNavItem>
+          </SectionNav>
         </BCol>
         <BCol>
           <ChallengeIntro
