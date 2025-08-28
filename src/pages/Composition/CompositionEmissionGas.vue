254d3caa67d1cec58edc28ba2929e980cb7a7d39
Pat Udomprasert <pudompra@cfa.harvard.edu>
2025-08-28 16:15:35 -0400

    Updates for Atomic Emission Part 3

--- src/pages/Composition/CompositionEmissionGas.vue
+++ src/pages/Composition/CompositionEmissionGas.vue
@@ -2,11 +2,32 @@
   <CompositionLayout>
     <template #challenge-tab>
       <ChallengeCard>
-        <InstructionHeader>Atomic emission: Part 2</InstructionHeader>
+        <InstructionHeader
+          >Atomic Emission Part 3: Identify Gases</InstructionHeader
+        >
         <InstructionRow row-type="Tool">
-          Identify the types of gas in the two lamps. Use the dropdown in the
-          top tool to switch between them. Compare them to the gases in the
-          bottom tool.
+          Identify the types of gas in the two light sources by comparing their
+          spectra with reference spectra for different elements.
+          <template #steps>
+            <InstructionStep>
+              Under <span class="challenge-tool-label">Source 1</span>, use the
+              dropdown menu to toggle between the spectra for the two light
+              sources.
+            </InstructionStep>
+            <InstructionStep>
+              Under <span class="challenge-tool-label">Source 2</span>, use the
+              dropdown menu to toggle between the spectra for different types of
+              gases.
+            </InstructionStep>
+            <InstructionStep>
+              Look for the best matches between the emission lines of the light
+              sources and the reference spectra to identify which gases are used
+              in the bulb and lamp.
+            </InstructionStep>
+          </template>
+        </InstructionRow>
+        <InstructionRow row-type="Notebook">
+          Answer the questions in your Notebook.
           <template #steps>
             <InstructionStep>
               What type of gas is in the metal halide bulb?
@@ -14,10 +35,13 @@
             <InstructionStep>
               What type of gas is in the vapor lamp?
             </InstructionStep>
+            <InstructionStep>
+              Identifying elements from the patterns in an emission line
+              spectrum takes a lot of practice. If you ran into any problems or
+              questions while trying to identify the elements in these light
+              bulbs, note them briefly here.
+            </InstructionStep>
           </template>
-        </InstructionRow>
-        <InstructionRow row-type="Notebook">
-          Write your answers in the <em>Atomic Line Emission</em> section.
         </InstructionRow>
       </ChallengeCard>
     </template>
@@ -35,12 +59,12 @@
       </ToolControlGroup>
       <LeftRightGroup class="mt-4">
         <template #left>
-          <NextPrevButton light direction="prev" to="lamps">
-            part 1
+          <NextPrevButton light direction="prev" to="slideshow">
+            part 2
           </NextPrevButton>
         </template>
         <template #right>
-          <NextPrevButton light direction="next" to="../atoms-and-spectra">
+          <NextPrevButton light direction="next" to="../atomic-absorption">
             next section
           </NextPrevButton>
         </template>
