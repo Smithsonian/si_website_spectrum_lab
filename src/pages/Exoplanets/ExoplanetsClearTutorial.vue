1e82a96c5fc1b9c208e319317d4e95846002afd2
Pat Udomprasert <pudompra@cfa.harvard.edu>
2025-09-04 15:12:51 -0400

    Text updates for Clear Atmospheres section

--- src/pages/Exoplanets/ExoplanetsClearTutorial.vue
+++ src/pages/Exoplanets/ExoplanetsClearTutorial.vue
@@ -2,30 +2,45 @@
   <ExoplanetsLayout>
     <template #challenge-tab>
       <ChallengeCard>
-        <InstructionHeader>Modeling Clear Atmospheres</InstructionHeader>
+        <InstructionHeader
+          >Modeling Clear Exoplanet Atmospheres</InstructionHeader
+        >
         <InstructionRow row-type="Intro">
           Astronomers predict the theoretical exoplanet spectrum they would
           expect to observe under ideal conditions. Each predictive model is
-          specific to a particular set of astmospheric conditions.
+          specific to a particular set of atmospheric conditions.
         </InstructionRow>
         <InstructionRow row-type="Tool">
-          Let’s explore some of these models, starting with the model for clear
-          skies.
-        </InstructionRow>
-        <InstructionRow row-type="Notebook">
-          Once you’ve completed the tutorial, go to the
-          <em>Tutorial: Modeling Exoplanet Atmospheres</em> section and answer
-          the following questions:
           <template #steps>
             <InstructionStep>
-              Why do you think astronomers create computer-generated predictions
-              of exoplanet atmospheres?
+              View the tutorial below, which describes a model for a Hot Jupiter
+              exoplanet with <span class="challenge-bold">clear</span> skies.
             </InstructionStep>
+            <InstructionStep>
+              Use the vertical measuring tool on the second step of the tutorial
+              to measure the wavelengths of the features for different atoms and
+              molecules to answer question 1 below.
+            </InstructionStep>
+            <InstructionStep>
+              Use the dropdown menu under
+              <span class="challenge-tool-label">Source 2</span> to compare
+              individual atomic and molecular spectra with the Hot Jupiter model
+              spectrum.
+            </InstructionStep>
+          </template>
+        </InstructionRow>
+        <InstructionRow row-type="Notebook">
+          As you view the tutorial, answer these questions in your Notebook.
+          <template #steps>
             <InstructionStep>
               What are the approximate center wavelengths of the Sodium (Na),
               Potassium (K), and three Water Vapor (H<sub>2</sub>O) features in
               the Comparison Atomic and Molecular spectra, and in the Clear Hot
               Jupiter model?
+            </InstructionStep>
+            <InstructionStep>
+              Why do you think astronomers create computer-generated predictions
+              of exoplanet atmospheres?
             </InstructionStep>
           </template>
         </InstructionRow>
@@ -62,7 +77,15 @@
       </ToolControlGroup>
       <LeftRightGroup v-if="tutorialState === 'nextSection'" class="mt-5">
         <template #left>
-          <NextPrevButton direction="prev" @click="replayResetZoom" light>
+          <NextPrevButton direction="prev" to="capturing-spectra" light>
+            previous section
+          </NextPrevButton>
+          <NextPrevButton
+            direction="prev"
+            @click="replayResetZoom"
+            light
+            class="ml-2"
+          >
             replay tutorial
           </NextPrevButton>
         </template>
